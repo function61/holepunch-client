0e1fcf108d0719e72a2a34e3d2167358252d0c53
Joonas Loppi <joonas@joonas.fi>
2020-03-05 13:40:49 +0200

    remove unused ENV vars from build script

--- bin/build.sh
+++ bin/build.sh
@@ -4,8 +4,5 @@ source /build-common.sh
 
 BINARY_NAME="holepunch"
 COMPILE_IN_DIRECTORY="cmd/holepunch"
-BINTRAY_PROJECT="function61/holepunch-client"
-
-INCLUDE_WINDOWS="true"
 
 standardBuildProcess
